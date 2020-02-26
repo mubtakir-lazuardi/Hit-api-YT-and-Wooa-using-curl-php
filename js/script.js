3104a0d97267087f7b0039601bde964930f3813c
brofillah <farhanscience@hotmail.com>
2020-02-26 10:55:20 +0700

    landing page

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//event pada saat klik link
+$('.page-scroll').on('click', function(e){
+
+    //ambil isi href
+    var tujuan = $(this).attr('href');
+    //ambil isi ybs
+    var elemenTujuan = $(tujuan);
+
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo')
+
+    e.preventDefault();
+});
+
+//about
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul')
+    $('.pKanan').addClass('pMuncul')
+})
+
+//paralax
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop()
+
+    //jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    })
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    })
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'      
+    })
+
+    //portfolio
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portfolio .thumbnail').eq(i).addClass('muncul')
+            },250 * (i+1))
+        })
+    }
+})
